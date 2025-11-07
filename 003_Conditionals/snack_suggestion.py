b448fbd98a78608600707872105bdcf47baa11d7
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2025-11-07 18:53:11 +0530

    building a snack system and learning conditional statements

---
+++ 003_Conditionals/snack_suggestion.py
@@ -0,0 +1,6 @@
+snack=input("enter a snack : ").lower()
+
+if snack=="cookies" or snack=="samosa":
+    print(f"ya , we will serve you {snack}")
+else:
+    print(f"we are not serving {snack} , try again any other day.")
