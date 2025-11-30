cc61e3178b1f24a6a9658ae0880d27c5579a98a5
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2025-11-30 15:44:53 +0530

    Add basic exceptions example script
    Introduces a script demonstrating an IndexError and lists common Python exceptions for educational purposes.

---
+++ 11_Exceptions/01_basic.py
@@ -0,0 +1,15 @@
+orders = ["masala", "ginger"]
+
+print(orders[2])
+
+#some popular exceptions:
+#IndexError - trying to access an index that does not exist
+#KeyError - trying to access a dictionary key that does not exist
+#TypeError - trying to perform an operation on incompatible types
+#ValueError - trying to convert a value to a type that is not compatible    
+#ZeroDivisionError - trying to divide a number by zero
+#NameError - trying to access a variable that has not been defined
+#AttributeError - trying to access an attribute that does not exist on an object
+#ImportError - trying to import a module that does not exist
+#ModuleNotFoundError - trying to import a module that cannot be found
+#SyntaxError - trying to run code that has invalid syntax
